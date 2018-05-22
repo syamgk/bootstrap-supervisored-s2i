bebe91bcda688b28900cce7ac9bf6a590863b48d
Tomas Kral <tkral@redhat.com>
2018-05-22 12:59:13 +0200

    run assemble script when container starts

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 # run s2i run script from source directory if it has been overridden  there
 if [ -f /opt/app-root/src/.s2i/bin/run ]; then
-    exec /opt/app-root/src/.s2i/bin/run``
+    exec /opt/app-root/src/.s2i/bin/run
 else
     exec /usr/libexec/s2i/run
 fi
